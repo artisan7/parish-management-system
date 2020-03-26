4b4ae2a155cd273372758007282e9968795ae81e
Dwight Ian De Jesus <ddejesus0320@gmail.com>
2020-03-26 13:23:55 +0800

    Removed the now ignored directory

--- mati-parish-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\adducs\django\parish-management-system\mati-parish-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
